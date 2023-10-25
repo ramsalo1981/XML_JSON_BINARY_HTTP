dd10b7fadaa102d9ba076198ff6c842f2bcdc914
ramsalo1981 <rami.salloum@hotmail.com>
2023-10-26 01:37:57 +0200

    first commit

---
+++ CAHttpClientJson/Program.cs
@@ -0,0 +1,35 @@
+﻿using System.Text.Json;
+
+namespace CAHttpClientJson
+{
+    class Program
+    {
+        private readonly static HttpClient httpClient = new HttpClient();
+        static async Task Main(string[] args)
+        {
+            var todoesJsonContent = await httpClient.GetStringAsync("https://jsonplaceholder.typicode.com/todos");
+
+            var todoes = JsonSerializer.Deserialize<List<Todo>>(todoesJsonContent
+                , new JsonSerializerOptions { PropertyNameCaseInsensitive = true });
+
+            foreach (var item in todoes)
+                Console.WriteLine(item);
+            Console.ReadKey();
+        }
+    }
+
+
+    public class Todo
+    {
+        public int Id { get; set; }
+        public int UserId { get; set; }
+        public string Title { get; set; }
+        public bool Completed { get; set; }
+
+
+        public override string ToString()
+        {
+            return $"\n [{Id} - UserId: {UserId}] -  {Title} {(Completed ? "completed" : "not completed")}";
+        }
+    }
+}
---
+++ CAXMLSerialization/Program.cs
@@ -0,0 +1,63 @@
+﻿using System.Xml;
+using System.Xml.Serialization;
+
+namespace CAXMLSerialization
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var e1 = new Employee
+            {
+                Id = 1001,
+                Fname = "Issam",
+                Lname = "A.",
+                Benefits = { "Pension", "Health Insurance" }
+            };
+
+            var xmlContent = SerializeToXmlString(e1);
+            Console.WriteLine(xmlContent);
+            File.WriteAllText("XmlDocument.xml", xmlContent);
+
+            var xmlContent2 = File.ReadAllText("xmlDocument.xml");
+            Employee e2 = DeserializeFromXmlString(xmlContent2);
+            
+            Console.ReadKey();  
+        }
+
+        // from string to Xml
+        private static string SerializeToXmlString(Employee e1)
+        {
+            var result = "";
+            var xmlSerializer = new XmlSerializer(e1.GetType());
+            using (var sw = new StringWriter())
+            {
+                using (var writer = XmlWriter.Create(sw, new XmlWriterSettings { Indent = true }))
+                {
+                    xmlSerializer.Serialize(writer, e1);
+                    result = sw.ToString();
+                }
+            }
+            return result;
+        }
+
+        // from xml to string
+        private static Employee DeserializeFromXmlString(string xmlContent)
+        {
+            Employee? employee = null;
+            var xmlSerializer = new XmlSerializer(typeof(Employee));
+            using (TextReader reader = new StringReader(xmlContent))
+            {
+                employee = xmlSerializer.Deserialize(reader) as Employee;
+            }
+            return employee;
+        }
+    }
+    public class Employee
+    {
+        public int Id { get; set; }
+        public string? Fname { get; set; }
+        public string? Lname { get; set; }
+        public List<string> Benefits { get; set; } = new List<string>();
+    }
+}
